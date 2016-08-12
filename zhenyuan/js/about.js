8f3e596507cddb944118d05264c436a1f416bca2
karl <ideexto@gmail.com>
2016-08-12 16:25:37 +0800

    Add some css and html files.

--- zhenyuan/js/about.js
+++ zhenyuan/js/about.js
@@ -2,5 +2,8 @@
  * Created by Karl on 2016/8/10.
  */
 $(function () {
-    
+    function resizeRender(){
+       $('.content').css('min-height', Math.max($(window).height() - 90 - 40, $('.menu').height()));
+    }
+    resizeRender();
 });
