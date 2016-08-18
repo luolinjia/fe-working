21a6c869ddd9af320cad0e7f1f97b88eb39ea751
luolinjia <360512239@qq.com>
2016-08-19 01:47:07 +0800

    Update the index page.

--- zhenyuan/js/case.js
+++ zhenyuan/js/case.js
@@ -67,4 +67,18 @@ $(function () {
         }
         return list.join('');
     }
+    $('.icon-wechat').on('click', function () {
+        var dialog = new ZY.Dialog({
+            title: '', //窗口标题的html，如果不设置则无标题
+            content: '<div><img src="img/wechat.png" /></div>',
+            //窗口内容的html，必须是html格式不能是无格式纯文本，如果不设置则无内容
+            beforeClose: null, //调用close方法时执行的callback，如果此callback返回false则会阻止窗口的关闭
+            showClose: true,
+            showFooter: false,
+            className: '', //窗口最外层容器的类名
+            cache: false, //是否缓存。若为false则close()的时候会remove掉对话框对应的dom元素
+            width: '311px' //窗口宽度，如不传递默认为40%
+        });
+        dialog.open();
+    });
 });
